56245e2e0bedaae2c9f59b296c5007d9c514b6a7
Denis Udovcic <denisudovcic@gmail.com>
2018-05-06 14:03:28 +0200

    express middleware instead...

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const app = express();
 
 app.use(bp.urlencoded({ extended: true}));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
+
 
 app.get('/events/', (req, res) => {
   request.get(`https://mock-api.drinks.test.siliconrhino.io/events?search=${req.query.search || ''}`, (err, request, body) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
     res.send(body);
   })
 });
